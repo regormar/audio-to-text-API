e84d51cc32970ddb21700597922d7efe8f431183
regormar <regormar99@gmail.com>
2024-12-02 19:21:00 +0100

    Fix app.js route

--- api/routes/audioRoutes.js
+++ api/routes/audioRoutes.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const multer = require("multer");
-const path = require("path");
-const { transcribeAudio } = require("../../../audio-to-text-API/api/controllers/audioController"); // Importar la función del controlador
+const { transcribeAudio } = require("../controllers/audioController");
 
 const router = express.Router();
-
-// Configuración de multer para manejar archivos de audio
 const upload = multer({ dest: "uploads/" });
 
-// Endpoint para transcribir audios
+// Endpoint para transcribir audios cargados
 router.post("/transcribe", upload.single("audio"), transcribeAudio);
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const audioRoutes = require("../business-backend/api/routes/audioRoutes");
+const cors = require("cors");
+const audioRoutes = require("../audio-to-text-API/api/routes/audioRoutes");
 
 const app = express();
 const PORT = 3000;
 
+
+// Configuración de CORS
+app.use(cors({
+  origin: "*", // Permitir todas las solicitudes. Cambiar según sea necesario.
+  methods: ["GET", "POST"], // Métodos permitidos.
+  allowedHeaders: ["Content-Type", "Authorization"], // Encabezados permitidos.
+}));
+
 // Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Routes
 app.use("/audio", audioRoutes);
